d32293fcf2d18cb9c38d7b8338ab92d7943c1922
mohamed rafik <114211230+mohamedrafik-lgtm@users.noreply.github.com>
2024-03-16 19:29:00 +0200

    remove todo

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -13,6 +13,8 @@ const TodoList = () => {
   const userData = userDataString ? JSON.parse(userDataString) : null
   
   const [isEditModelOpen,setEditModelOpen] = useState(false)
+  const [isUpdating,setIsUpdating] = useState(false)
+  const [deleteModel,setDeleteModel] = useState(false)
   const [todoToEdite,setTodoToEdit] = useState<ITodo>({
       id: 0,
       title: "",
@@ -35,7 +37,7 @@ const TodoList = () => {
     setEditModelOpen(true)
   }
   const {isLoading,data} = useAuthenticatedQuery({
-    queryKey:["todos"],
+    queryKey:["todoList",`${todoToEdite.id}`],
     url:"/users/me?populate=todos",
     config:{
       headers:{
@@ -43,6 +45,14 @@ const TodoList = () => {
       }
     }
   })
+
+
+  const openDeleteModel = ()=>{
+    setDeleteModel(true)
+  }
+  const closeDeleteModel = ()=>{
+    setDeleteModel(false)
+  }
 
   const onChangeHandler = ( evt :ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
     const {value,name} = evt.target
@@ -52,19 +62,24 @@ const TodoList = () => {
     })
   }
   const submitHandler = async (evt:FormEvent<HTMLFormElement>) =>{
+    setIsUpdating(true)
     evt.preventDefault()
     const {title,description} = todoToEdite;
     try {
-    const res =  await axiosInstance.put(`/todos/${todoToEdite.id}`,{
+    const {status} =  await axiosInstance.put(`/todos/${todoToEdite.id}`,{
       data:{title,description}
     },{
       headers:{
         Authorization:`Bearer ${userData.jwt}`
       }
     })
-    console.log(res)
+    if (status === 200){
+      onCloseEditModil()
+    }
     } catch (error) {
       console.log(error)
+    }finally{
+      setIsUpdating(false)
     }
   }
   
@@ -78,7 +93,7 @@ const TodoList = () => {
         <p className="w-full font-semibold">1 - {todo.title}</p>
         <div className="flex items-center justify-end w-full space-x-3">
           <Button size={"sm"} onClick={()=> onOpenEditModil(todo)}>Edit</Button>
-          <Button variant={"danger"} size={"sm"}>
+          <Button variant={"danger"} size={"sm"} onClick={openDeleteModel}>
             Remove
           </Button>
         </div>
@@ -90,12 +105,26 @@ const TodoList = () => {
         <Input name="title" value={todoToEdite.title} onChange={onChangeHandler}/>
         <Textarea name="description" value={todoToEdite.description} onChange={onChangeHandler}/>
         <div className="flex items-center space-x-2 mt-4">
-        <Button className="bg-indigo-700 hover:bgidigo-800">Update</Button>
+        <Button className="bg-indigo-700 hover:bgidigo-800" isLoading={isUpdating}>Update</Button>
         <Button variant={"cancel"} onClick={onCloseEditModil}>
           Cancel
         </Button>
         </div>
         </form>
+      </Modal>
+
+      <Modal isOpen={deleteModel}
+      closeModal={closeDeleteModel}
+      title="are you sure you want to remove this Todo from your store?"
+      description="deleting this todo will remove it permanently from your inventory. any associated data, sales history and other related information
+      will also be deleted , plase make sure this is the intended action."
+      >
+        <div className="flex items-center space-x-3">
+          <Button variant={"danger"} onClick={()=>{}}>
+            Yes. remove
+            </Button>
+            <Button variant={"cancel"} onClick={closeDeleteModel}>Cansel</Button>
+        </div>
       </Modal>
       </div>
     
