e90188f42b8419be52eafe86c88e7f044943cb66
mohamed rafik <114211230+mohamedrafik-lgtm@users.noreply.github.com>
2024-03-18 18:55:03 +0200

    Add A New Todo

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -18,12 +18,29 @@ const TodoList = () => {
   const [isUpdating,setIsUpdating] = useState(false)
   const [deleteModel,setDeleteModel] = useState(false)
   const [todoToEdite,setTodoToEdit] = useState<ITodo>({
-      id: 0,
-      title: "",
-      description:""
+    id: 0,
+    title: "",
+    description:""
   })
+  const [todoToAdd,setTodoToAdd] = useState({
+    title: "",
+    description:""
+  })
+  const [isOpenAddModel,setIsAddModel] = useState(false)
 
   // Handelers
+  const onCloseAddModil= () => {
+    setTodoToAdd(
+      {
+        title: "",
+        description:""
+    }
+    )
+    setIsAddModel(false)
+  };
+  const onOpenAddModil= () => {
+    setIsAddModel(true)
+  }
   const onCloseEditModil= () => {
     setTodoToEdit(
       {
@@ -70,6 +87,13 @@ const TodoList = () => {
       [name]:value
     })
   }
+  const onChangeAddHandler = ( evt :ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+    const {value,name} = evt.target
+    setTodoToAdd({
+      ...todoToAdd,
+      [name]:value
+    })
+  }
   const onRemove = async ()=>{
     try {
     const {status} =  await axiosInstance.delete(`/todos/${todoToEdite.id}`,{
@@ -99,7 +123,39 @@ const TodoList = () => {
     if (status === 200){
       setTimeout(()=>{
         onCloseEditModil()
-        toast.success("It will be deleted after 500 ms", {
+        toast.success("It will be deleted this todo after 500 ms", {
+          position: "bottom-center",
+          duration: 1500,
+          style: {
+            backgroundColor: "black",
+            color: "white",
+            width: "fit-content",
+          },
+        })
+      },500)
+    }
+    } catch (error) {
+      console.log(error)
+    }finally{
+      setIsUpdating(false)
+    }
+  }
+  const submitAddTodoHandler = async (evt:FormEvent<HTMLFormElement>) =>{
+    setIsUpdating(true)
+    evt.preventDefault()
+    const {title,description} = todoToAdd;
+    try {
+    const {status} =  await axiosInstance.post(`/todos`,{
+      data:{title,description}
+    },{
+      headers:{
+        Authorization:`Bearer ${userData.jwt}`
+      }
+    })
+    if (status === 200){
+      setTimeout(()=>{
+        onCloseAddModil()
+        toast.success("It will be deleted this todo after 500 ms", {
           position: "bottom-center",
           duration: 1500,
           style: {
@@ -124,8 +180,10 @@ const TodoList = () => {
   </div>
   </>
   return (
-    
     <div className="space-y-1 ">
+      <div className="w-fit mx-auto my-10">
+          <Button size={"sm"} onClick={onOpenAddModil}>Post new todo</Button>
+      </div>
       {data.todos.length ? data.todos.map((todo:ITodo) => (
         <div key={todo.id} className="flex items-center justify-between hover:bg-gray-100 duration-300 p-3 rounded-md even:bg-gray-100">
         <p className="w-full font-semibold">{todo.id}- {todo.title}</p>
@@ -137,7 +195,20 @@ const TodoList = () => {
         </div>
       </div>
       )) : <h3>No todos yet!</h3>}
-      
+      {/* add todo  */}
+      <Modal isOpen={isOpenAddModel} closeModal={onCloseAddModil} title="Add A new todo">
+        <form className="space-y-3" onSubmit={submitAddTodoHandler}>
+        <Input name="title" value={todoToAdd.title} onChange={onChangeAddHandler}/>
+        <Textarea name="description" value={todoToAdd.description} onChange={onChangeAddHandler}/>
+        <div className="flex items-center space-x-2 mt-4">
+        <Button className="bg-indigo-700 hover:bgidigo-800" isLoading={isUpdating}>Add Todo</Button>
+        <Button variant={"cancel"} onClick={onCloseEditModil}>
+          Cancel
+        </Button>
+        </div>
+        </form>
+      </Modal>
+      {/* edit model */}
       <Modal isOpen={isEditModelOpen} closeModal={onCloseEditModil} title="edit this todo">
         <form className="space-y-3" onSubmit={submitHandler}>
         <Input name="title" value={todoToEdite.title} onChange={onChangeHandler}/>
@@ -150,7 +221,7 @@ const TodoList = () => {
         </div>
         </form>
       </Modal>
-
+          
       <Modal isOpen={deleteModel}
       closeModal={closeDeleteModel}
       title="are you sure you want to remove this Todo from your store?"
